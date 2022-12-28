210429c7fa0746ea49efc3b621c72a9364673ff8
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-12-28 14:57:05 -0300

    :rocket: Criando a estrutura da roda de criação de produtos

--- BackEnd/src/routes.ts
+++ BackEnd/src/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
-
+// -- User --
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailsUserController } from './controllers/user/DetailsUserController';
-
+// -- Middleware --
 import { isAuthenticated } from './middleware/isAuthenticated';
-
+// -- Category --
 import {CreateCategoryController} from './controllers/category/CreateCategoryController';
 import {ListCategoryController} from './controllers/category/ListCategoryController';
+// -- Products --
+import {CreateProductController} from './controllers/product/CreateProductController';
 
 const router = Router();
 
@@ -25,5 +27,8 @@ router.get('/info', isAuthenticated, new DetailsUserController().handle);
 //-- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+//-- ROTAS PRODUCTS --
+router.post('/product', isAuthenticated, new CreateProductController().handle);
 
 export { router };
