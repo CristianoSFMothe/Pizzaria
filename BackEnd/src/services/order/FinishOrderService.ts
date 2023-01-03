0ef0b57fa258bb0af14b4038da7bfbd7d87e0eff
Cristiano Ferreira <cristianodevsystem@gmail.com>
2023-01-03 12:09:32 -0300

    :rocket: Concluir o pedido

---
+++ BackEnd/src/services/order/FinishOrderService.ts
@@ -0,0 +1,25 @@
+import prismaClient from '../../prisma';
+
+interface IOrderRequest {
+  order_id: string;
+}
+class FinishOrderService {
+  async execute({ order_id }: IOrderRequest) {
+    const order = await prismaClient.order.update({
+      where: {
+        id: order_id
+      },
+      data: {
+        status: true,
+      }
+    });
+
+    return {
+      data: {
+        order,
+      }
+    };
+  }
+}
+
+export {FinishOrderService};
